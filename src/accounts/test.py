faacb08997243576124a7f38494074447ef13be5
espaguetissecos <espaguetissintomate@gmail.com>
2017-11-22 23:42:55 +0100

    updated

--- src/accounts/test.py
+++ src/accounts/test.py
@@ -1,8 +1,11 @@
 import os
+
+# !!!!!!!!!!!!!!! ES NECESARIO ANYADIR A LA VARIABLE PATH geckodriver
+
 os.environ['TBB_PATH'] = '/home/kiko/TFG/libraries/geckodriver/tor-browser_en-US/'
 from tbselenium.tbdriver import TorBrowserDriver
 script_dir = os.path.dirname(__file__)
-path = os.path.join(os.path.join(script_dir, os.pardir), '../libraries/geckodriver/tor-browser_en-US/start-tor-browser.desktop')
+path = os.path.join(os.path.join(script_dir, os.pardir), '../libraries/geckodriver/tor-browser_en-US/')
 
-with TorBrowserDriver("path") as driver:
+with TorBrowserDriver(path) as driver:
     driver.get('https://check.torproject.org')
