bcafbc125c5a2a88fcf8c970f6e55001cf9c99b9
espaguetissecos <espaguetissintomate@gmail.com>
2017-11-23 23:39:37 +0100

    TOR working on Selenium

--- src/accounts/driver_builder.py
+++ src/accounts/driver_builder.py
@@ -1,9 +1,9 @@
 import os
-
-# !!!!!!!!!!!!!!! ES NECESARIO ANYADIR A LA VARIABLE PATH geckodriver
-
 os.environ['TBB_PATH'] = '/home/kiko/TFG/libraries/geckodriver/tor-browser_en-US/'
 from tbselenium.tbdriver import TorBrowserDriver
+
+# !!!!!!!!!!!!!!! ES NECESARIO ANYADIR A LA VARIABLE PATH la ruta del geckodriver
+
 script_dir = os.path.dirname(__file__)
 path = os.path.join(os.path.join(script_dir, os.pardir), '../libraries/geckodriver/tor-browser_en-US/')
 
