ba813f090be05bde6f6f53864fbd6966aa1cf797
darg <darg@uam.es>
2018-01-16 11:31:26 +0100

    erratas eliminadas II

--- Memoria/auto/PLANTILLA_TFG.el
+++ Memoria/auto/PLANTILLA_TFG.el
@@ -5,14 +5,14 @@
                      '(("book" "a4paper" "11pt" "twoside" "openright" "titlepage")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("geometry" "a4paper" "left=1in" "right=1in" "top=0.6in") ("inputenc" "utf8") ("fontenc" "T1") ("babel" "english" "spanish") ("xy" "all") ("xcolor" "table" "xcdraw") ("url" "hyphens") ("hyperref" "breaklinks")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
    (TeX-run-style-hooks
     "latex2e"
     "portada"
@@ -57,6 +57,7 @@
     "fancybox"
     "breakurl"
     "hyperref"
+    "lscape"
     "fancyhdr")
    (TeX-add-symbols
     '("commentswho" 2)
